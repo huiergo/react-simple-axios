69b14738420e503d9b2b84406ead78b83d50c2c5
dinghui1 <dinghui1@itcast.cn>
2023-03-20 18:49:51 +0800

    init proxy

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+async function getAllUsers() {
+
+  const response = await fetch('/api/users');
+  return await response.json();
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={() => {
+          console.log(123)
+          getAllUsers()
+            .then(users => {
+              console.log(users)
+            });
+        }}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
